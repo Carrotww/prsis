a2f1cd8fb9a74be4912aa0653e21f937494806a7
Carrot_hyeong <rjgjfl90@naver.com>
2025-04-07 21:48:43 +0900

    project prsis init

---
+++ src/main/java/teamzeppy/prasis/PrasisApplication.java
@@ -0,0 +1,13 @@
+package teamzeppy.prasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrasisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teamzeppy/prasis/PrasisApplicationTests.java
@@ -0,0 +1,13 @@
+package teamzeppy.prasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
